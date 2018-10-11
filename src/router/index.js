ec1b94363052a9d3c23eb33b0644445d35136ff9
lixuwei <18686846532@163.com>
2018-10-11 09:47:36 +0800

    elementUI列表拖拽功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Spin from '@/components/spin'
+import Spin1 from '@/components/spin1'
+import DragDemo from '@/components/drag_demo'
 
 Vue.use(Router)
 
@@ -10,6 +12,16 @@ export default new Router({
       path: '/',
       name: 'Spin',
       component: Spin
-    }
+    },
+    {
+      path: '/Spin1',
+      name: 'Spin1',
+      component: Spin1
+    },
+    {
+      path: '/dragDemo',
+      name: 'DragDemo',
+      component: DragDemo
+    },
   ]
 })
