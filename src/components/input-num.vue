3483630f00f42b16cd8166c6d396126229a9bbd9
lixuwei <18686846532@163.com>
2018-11-30 17:48:02 +0800

    num-input

---
+++ src/components/input-num.vue
@@ -0,0 +1,39 @@
+<template>
+	<div id="inputNum">
+		<div class="ceiling">测试numInput</div>
+		<numInput style="width: 50%; margin-left: 20px" v-model="num" :value="num" :numLen='10' :max="10"></numInput>
+		<div style="padding-left: 20px; text-align: left">
+			<input placeholder="输入字母，汉子，符号 click给numInput" v-model="aa" style="padding: 4px;width: 260px"> 
+			<button @click='aaa' style="padding: 4px 10px">click</button>
+		</div>
+	</div>
+</template>
+<script>
+	import numInput from './numInput.vue'
+	export default{
+		name: 'input-num',
+		data() {
+			return {
+				num: '',
+				aa: '',
+			}
+		},
+		methods: {
+			aaa() {
+				this.num = this.aa;
+			},
+		},
+		components: {
+			numInput,
+		}
+	}
+</script>
+<style type="text/css" scoped>
+	.ceiling {
+		font-size: 18px;
+		line-height: 50px;
+		text-align: center;
+		background: #2db7f5;
+		font-weight: 900;
+	}
+</style>
--- src/components/numInput.vue
+++ src/components/numInput.vue
@@ -1,14 +1,20 @@
 <template>
   <div id="numInput">
-    <el-input v-model="val"
-              @input="limitNum(val, 'val', 0, '')"
-    ></el-input>
+    <el-input placeholder="numInput" v-model="currentValue" @input="limitNum"></el-input>
   </div>
 </template>
 <script>
   export default {
     name: "numInput",
     props: {
+      value: {
+        type: [String, Number],
+        default: ''
+      },
+      value: {
+        type: String,
+        default: 'numInput'
+      },
       max: {
         type: Number,
         default: Infinity
@@ -17,46 +23,74 @@
         type: Number,
         default: -Infinity
       },
-      value: '',
-      numType: {// 数据类型 小数 整数
+      numLen: {// 小数点算一位，用户可输入的长度
+        type: Number,
+      },
+      precision: {// 几位小数
+        type: Number,
+      },
+      numType: {// 不会出现 + 号
         type: String,
-        default: 'integer',
+        default: ''
+        // {
+        //    "无限制": '',
+        //    "整数": 'integer',
+        //    "正数": 'plus',
+        // }
+        //    "小数": 'decimals',
+        //    "负数": 'minus'
       }
     },
     data() {
       return {
-        val: this.value,
         currentValue: '',
+        oldValue: '',
+      }
+    },
+    watch: {
+      value() {
+        console.log(isNaN(this.value));
+        this.currentValue = this.value;
+        this.limitNum();
       }
     },
     methods: {
-      // input 输入数字处理
-      limitNum(val, attr, min) {
-        val = val.replace(/\++/g, '');// 去除 '+'
-        console.log(isNaN(val));
-
-
-
-
-        if (!isNaN(val)) {
-          let oldCurrentValue = this.currentValue;
-          this.currentValue = val.replace(/(\b0+)/, '');
-          // 最小值边界
-          if (this.currentValue < min) this.currentValue = oldCurrentValue;
-          // 正整数
-          if (this.numType === 'integer') this.currentValue = this.currentValue.replace(/(\.+)/, '');
-          this.$nextTick(() => {
-            this[attr] = this.currentValue;
-          })
-        }
-        else {
-          this.$nextTick(() => {
-            this[attr] = this.currentValue;
-          })
-        }
+      assignment(newVal) {
+        this.$nextTick(() => {
+          this.currentValue = newVal;
+        })
       },
+      // 数字输入框
+      limitNum() {
+        const {min, max, numType, numLen} = this;
+        let val = this.currentValue;
+        if (val.length > numLen) {
+          this.assignment(this.oldValue);
+          return;
+        }
+        console.log(this.oldValue);
+        if (val.match(/^0{1}$/)) return;// 首位0
+        if (numType !== 'plus' && val.match(/^\-{1}$/)) return;// 正数
+        if (numType !== 'integer' && val.match(/^\.{1}$/)) return;// 整数
+        if (!isNaN(val)) {
+          if (!val.match(/\.+/)) val = val.replace(/(\b0+)/, '');
+          this.oldValue = val;
+          // 最小值边界
+          if (val < min) val = min;
+          // 最打值边界
+          if (val > max) val = max;
+          // 整数
+          if (numType === 'integer') val = val.replace(/(\.+)/, '');
+          this.assignment(val);
+        }
+        else this.assignment(this.oldValue);
+      },
+      blurFunc() {},
     }
   }
 </script>
 <style scoped>
+  #numInput{
+    margin: 10px 0;
+  }
 </style>
